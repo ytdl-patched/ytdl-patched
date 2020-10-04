03c95628dfe8bbf54ba2f2ed2e01bb2ba9d4f31a
nao20010128nao <nao20010128@gmail.com>
2020-10-04 08:35:15 +0000

    Change Jython URL

--- devscripts/install_jython.sh
+++ devscripts/install_jython.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget http://central.maven.org/maven2/org/python/jython-installer/2.7.1/jython-installer-2.7.1.jar
-java -jar jython-installer-2.7.1.jar -s -d "$HOME/jython"
+wget https://repo1.maven.org/maven2/org/python/jython-installer/2.7.2/jython-installer-2.7.2.jar
+java -jar jython-installer-2.7.2.jar -s -d "$HOME/jython"
 $HOME/jython/bin/jython -m pip install nose
