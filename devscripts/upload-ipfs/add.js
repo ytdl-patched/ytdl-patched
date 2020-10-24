d3607abcfd545a0f226fdf8805dec4bd894542e1
nao20010128nao <nao20010128@gmail.com>
2020-10-24 14:07:38 +0000

    upload to infura and local node

---
+++ devscripts/upload-ipfs/add.js
@@ -0,0 +1,25 @@
+// this assumes to be run inside ipfs.yml
+
+const ipfsHttp = require("ipfs-http-client");
+const { globSource } = ipfsHttp;
+
+const clients = [
+  ipfsHttp("http://127.0.0.1:5001"),
+  ipfsHttp("https://ipfs.infura.io:5001"),
+];
+
+async function main() {
+  const results = await Promise.all(
+    clients.map((a) => a.add(globSource("./ytdl-patched", { recursive: true })))
+  );
+  console.log(results);
+  const cid = results[1].cid.toString();
+  console.log(`::set-output name=ipfs-hash::${cid}`);
+}
+
+main()
+  .then(() => {})
+  .catch((a) => {
+    console.log(a);
+    process.exit(1);
+  });
