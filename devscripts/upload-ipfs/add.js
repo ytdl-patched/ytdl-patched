a6bd80a4d97c53cc66126bee3537277ef6040c16
nao20010128nao <nao20010128@gmail.com>
2020-10-24 15:50:40 +0000

    don't rely on local node that isn't reliable

--- devscripts/upload-ipfs/add.js
+++ devscripts/upload-ipfs/add.js
@@ -4,16 +4,21 @@ const ipfsHttp = require("ipfs-http-client");
 const { globSource } = ipfsHttp;
 
 const clients = [
-  ipfsHttp("http://127.0.0.1:51234"),
+  ipfsHttp("http://127.0.0.1:5001"),
   ipfsHttp("https://ipfs.infura.io:5001"),
+  ipfsHttp("https://ipfs.oceanprotocol.com/ipfs/"),
 ];
 
 async function main() {
-  const results = await Promise.all(
-    clients.map((a) => a.add(globSource("./ytdl-patched", { recursive: true })))
-  );
+  const results = (
+    await Promise.all(
+      clients
+        .map((a) => a.add(globSource("./ytdl-patched", { recursive: true })))
+        .map((a) => a.catch(() => false))
+    )
+  ).filter((a) => a);
   console.log(results);
-  const cid = results[1].cid.toString();
+  const cid = results[0].cid.toString();
   console.log(`::set-output name=ipfs-hash::${cid}`);
 }
 
