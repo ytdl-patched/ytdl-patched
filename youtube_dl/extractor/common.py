ytdl-org/youtube-dl

commit 6ad0d8781e2b156a4599abcebaa0b91b7e3131f4

[extractor/common] Fix media type extraction for HTML5 media tags in start/end form

youtube_dl/extractor/common.py
@@ -2521,8 +2521,8 @@ class InfoExtractor(object):
             # Allowing more characters may end up in significant slow down (see
             # https://github.com/ytdl-org/youtube-dl/issues/11979, example URL:
             # http://www.porntrex.com/maps/videositemap.xml).
-            r'(?s)(<(?P<tag>(?:amp-)?(?:video|audio))(?:\s+[^>]*)?>)(.*?)</(?P=tag)>', webpage))
-        for media_tag, media_type, media_content in media_tags:
+            r'(?s)(<(?P<tag>(?:amp-)?(video|audio))(?:\s+[^>]*)?>)(.*?)</(?P=tag)>', webpage))
+        for media_tag, _, media_type, media_content in media_tags:
             media_info = {
                 'formats': [],
                 'subtitles': {},

ytdl-patched/ytdl-patched

commit b0166e610a8e999cec463a2d657283b2e3605611

Fix test failure caused by 6ad0d8781e2b156a4599abcebaa0b91b7e3131f4

.gitignore
@@ -21,6 +21,7 @@ youtube-dl.tar.gz
 cover/
 updates_key.pem
 .jython_cache/
+.pytest_cache/
 *.egg-info
 *.srt
 *.ttml

youtube_dl/extractor/common.py
@@ -2549,7 +2549,7 @@ class InfoExtractor(object):
         # https://www.ampproject.org/docs/reference/components/amp-video)
         # For dl8-* tags see https://delight-vr.com/documentation/dl8-video/
         _MEDIA_TAG_NAME_RE = r'(?:(?:amp|dl8(?:-live)?)-)?(video|audio)'
-        media_tags = [(media_tag, media_type, '')
+        media_tags = [(media_tag, '', media_type, '')
                       for media_tag, media_type
                       in re.findall(r'(?s)(<%s[^>]*/>)' % _MEDIA_TAG_NAME_RE, webpage)]
         media_tags.extend(re.findall(