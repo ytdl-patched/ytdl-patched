8aa734ec1848e255ec9fd063d303331153d5dd3f
nao20010128nao <nao20010128@gmail.com>
2020-12-24 04:44:04 +0000

    [javhub] implement rest of extractor

--- youtube_dl/extractor/javhub.py
+++ youtube_dl/extractor/javhub.py
@@ -2,22 +2,18 @@
 from __future__ import unicode_literals
 
 import re
-import itertools
 
 from .common import InfoExtractor
-from ..compat import (
-    compat_urllib_parse_urlparse,
-)
 from ..utils import (
-    int_or_none,
-    mimetype2ext,
-    url_or_none,
-    urljoin,
+    ExtractorError,
+    determine_ext,
+    decode_base,
+    encode_base,
+    scalar_to_bytes,
 )
 
 
 class JavhubIE(InfoExtractor):
-    # https://gist.github.com/nao20010128nao/ea66c03691fc253e4182deb350560cad
     IE_NAME = 'javhub'
     _VALID_URL = r'https://(?:ja\.)?javhub\.net/play/(?P<id>[^/]+)'
 
@@ -28,5 +24,53 @@ class JavhubIE(InfoExtractor):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
-        data_src = self._search_regex(r'data-src="([\"]+)"(?:>| data-track)', webpage, 'data-src')
-        data_track = self._search_regex(r'data-track="([\"]+)">', webpage, 'data-track', fatal=False)
+        data_src = self._search_regex(r'data-src="([^\"]+)"(?:>| data-track)', webpage, 'data-src')
+        data_track = self._search_regex(r'data-track="([^\"]+)">', webpage, 'data-track', fatal=False)
+
+        playapi_post = encode_base(decode_base(data_src, self.B58_TABLE_2), self.B58_TABLE_1).encode('utf8')
+        if data_track:
+            webvtt_url = scalar_to_bytes(decode_base(data_track, self.B58_TABLE_2)).decode('utf8')
+        else:
+            webvtt_url = None
+
+        playapi_response = self._download_json(
+            'https://ja.javhub.net/playapi', video_id,
+            data=b'data=%s' % playapi_post, headers={'Referer': url})
+        mp4_url = playapi_response.get('playurl')
+        if not mp4_url:
+            raise ExtractorError('Server returned invalid data')
+        mp4_url = scalar_to_bytes(decode_base(mp4_url, self.B58_TABLE_2)).decode('utf8')
+
+        result = self._search_json_ld(webpage, video_id)
+        title = self._search_regex(r'<h5[^>]+?>(.+)</h5>', webpage, 'title', default=None)
+        if not title and result.get('title'):
+            title = result['title']
+        if not title:
+            title = self._html_extract_title(webpage, 'html title', default=None, fatal=False)
+
+        if title:
+            title = re.sub(r'^(?:無料動画|Watch Free Jav)\s+', '', title)
+
+        result.update({
+            'id': video_id,
+            'title': title,
+            'formats': [{
+                'url': mp4_url,
+                'protocol': 'http',
+                'ext': determine_ext(mp4_url),
+                'http_headers': {
+                    'Referer': url,
+                },
+            }],
+        })
+        if webvtt_url:
+            result.update({
+                'subtitles': {
+                    # unknown language
+                    'xx': [{
+                        'url': webvtt_url,
+                        'ext': determine_ext(webvtt_url),
+                    }]
+                },
+            })
+        return result
