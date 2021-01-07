ac61d4fdebc906320a85165fcf8dfdbc39c87563
nao20010128nao <nao20010128@gmail.com>
2021-01-07 06:49:55 +0000

    [y2mate] refactor this insanity

--- youtube_dl/extractor/y2mate.py
+++ youtube_dl/extractor/y2mate.py
@@ -4,42 +4,49 @@ from __future__ import unicode_literals
 import re
 
 from ..compat import compat_urllib_parse_urlencode
-from ..utils import ExtractorError, int_or_none, parse_filesize
+from ..utils import (
+    ExtractorError,
+    int_or_none,
+    parse_filesize,
+    urlencode_postdata,
+)
 from .common import InfoExtractor
 from .youtube import YoutubeIE
 
 
 class Y2mateIE(InfoExtractor):
-    _VALID_URL = r'(?x)^(?:y2(?:mate)?:|https?:\/\/(?:www\.)y2mate\.com\/(?:.+\/)?youtube\/)%s' % re.sub(r'^\(\?x\)\^', '', YoutubeIE._VALID_URL)
+    _VALID_URL = r'(?x)^(?:y2(?:mate)?:|https?://(?:www\.)y2mate\.com/(?:.+/)?youtube/)%s' % re.sub(r'^\(\?x\)\^', '', YoutubeIE._VALID_URL)
     IE_NAME = 'y2mate'
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         self._download_webpage('https://www.y2mate.com/youtube/%s' % video_id, video_id)
-        request_data = {
+        common_headers = {'X-Requested-With': 'XMLHttpRequest', 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+        request_data = urlencode_postdata({
             'url': 'https://www.youtube.com/watch?v=%s' % video_id,
             'q_auto': '1',
             'ajax': '1'
-        }
-        size_specs = self._download_json('https://www.y2mate.com/mates/analyze/ajax', video_id,
-                                         note='Fetching size specs', errnote='This video is unavailable', data=compat_urllib_parse_urlencode(request_data).encode('utf-8'),
-                                         headers={'X-Requested-With': 'XMLHttpRequest', 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'})
+        })
+        size_specs = self._download_json(
+            'https://www.y2mate.com/mates/analyze/ajax', video_id,
+            note='Fetching size specs', errnote='This video is unavailable', data=request_data,
+            headers=common_headers)
         if size_specs.get('status') != 'success':
             raise ExtractorError('Server responded with status %s' % size_specs.get('status'))
         size_specs = size_specs['result']
-        title = self._search_regex(r'<b>(.+?)<\/b>', size_specs, 'video title', group=1)
+        title = self._search_regex(r'<b>(.+?)</b>', size_specs, 'video title', group=1)
         request_id = self._search_regex(r'var k__id\s*=\s*(["\'])(.+?)\1', size_specs, 'request ID', group=2)
         formats = []
         # video    , mp3, audio
         video_table, _, audio_table = re.findall(r'<table\s*.+?>(.+?)</table>', size_specs)
 
         for rows in re.finditer(r'''(?x)<tr>\s*
-                    <td>.+?(\d+p).+?<\/td>\s* # resolution name
-                    <td>(.*?\s*[kMG]?B)<\/td>\s* # estimate size
-                    <td\s*.+?>.+?(?:data-ftype="(.+?)".+?)?(?:data-fquality="(.+?)".+?)?<\/td>\s* # download button
-                <\/tr>''', video_table):
+                    <td>.+?(\d+p).+?</td>\s* # resolution name
+                    <td>(.*?\s*[kMG]?B)</td>\s* # estimate size
+                    <td\s*.+?>.+?(?:data-ftype="(.+?)".+?)?(?:data-fquality="(.+?)".+?)?</td>\s* # download button
+                </tr>''', video_table):
             format_name, estimate_size, format_ext, request_format = rows.groups()
-            request_data = {
+            request_data = urlencode_postdata({
                 'type': 'youtube',
                 '_id': request_id,
                 'v_id': video_id,
@@ -47,22 +54,22 @@ class Y2mateIE(InfoExtractor):
                 'token': '',
                 'ftype': format_ext,
                 'fquality': request_format,
-            }
-            for i in range(10):
+            })
+            for i in range(5):
                 url_data = self._download_json(
                     'https://www.y2mate.com/mates/convert', video_id,
-                    note='Fetching infomation for %s' % format_name, data=compat_urllib_parse_urlencode(request_data).encode('utf-8'),
-                    headers={'X-Requested-With': 'XMLHttpRequest', 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'})
+                    note='Fetching infomation for %s' % format_name, data=request_data,
+                    headers=common_headers)
                 if url_data.get('status') != 'success':
                     self.report_warning('Server responded with status %s' % url_data.get('status'))
                     continue
                 url = self._search_regex(
                     r'<a\s+(?:[a-zA-Z-_]+=\".+?\"\s+)*href=\"(https?://.+?)\"(?:\s+[a-zA-Z-_]+=\".+?\")*', url_data['result'],
-                    video_id, 'Download url for %s' % format_name, group=1, default=None)
+                    'Download url for %s' % format_name, group=1, default=None)
                 if url and url.startswith('http'):
                     break
                 else:
-                    self.report_warning('URL error, retrying %d of 10' % i)
+                    self.report_warning('URL error, retrying %d of 5' % (i + 1))
                     url = None
 
             if not url:
@@ -80,10 +87,10 @@ class Y2mateIE(InfoExtractor):
             })
 
         for rows in re.finditer(r'''(?x)<tr>\s*
-                    <td>.+?(\d+[kMG]?bps).+?<\/td>\s* # resolution name
-                    <td>(.*?\s*[kMG]?B)<\/td>\s* # estimate size
-                    <td\s*.+?>.+?(?:data-ftype="(.+?)".+?)?(?:data-fquality="(.+?)".+?)?<\/td>\s* # download button
-                <\/tr>''', audio_table):
+                    <td>.+?(\d+[kMG]?bps).+?</td>\s* # resolution name
+                    <td>(.*?\s*[kMG]?B)</td>\s* # estimate size
+                    <td\s*.+?>.+?(?:data-ftype="(.+?)".+?)?(?:data-fquality="(.+?)".+?)?</td>\s* # download button
+                </tr>''', audio_table):
             format_name, estimate_size, format_ext, request_format = rows.groups()
             request_data = {
                 'type': 'youtube',
@@ -94,21 +101,21 @@ class Y2mateIE(InfoExtractor):
                 'ftype': format_ext,
                 'fquality': request_format,
             }
-            for i in range(10):
+            for i in range(5):
                 url_data = self._download_json(
                     'https://www.y2mate.com/mates/convert', video_id,
                     note='Fetching infomation for %s' % format_name, data=compat_urllib_parse_urlencode(request_data).encode('utf-8'),
-                    headers={'X-Requested-With': 'XMLHttpRequest', 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'})
+                    headers=common_headers)
                 if url_data.get('status') != 'success':
                     self.report_warning('Server responded with status %s' % url_data.get('status'))
                     continue
                 url = self._search_regex(
                     r'<a\s+(?:[a-zA-Z-_]+=\".+?\"\s+)*href=\"(https?://.+?)\"(?:\s+[a-zA-Z-_]+=\".+?\")*', url_data['result'],
-                    video_id, 'Download url for %s' % format_name, group=1, default=None) or ''
+                    'Download url for %s' % format_name, group=1, default=None) or ''
                 if url.startswith('http'):
                     break
                 else:
-                    self.report_warning('URL error: %s, retrying %d of 10' % i)
+                    self.report_warning('URL error: %s, retrying %d of 5' % (i + 5))
                     url = None
 
             if not url:
