ab5abfd0af57a4aef9711ef514b7425d79b69230
nao20010128nao <nao20010128@gmail.com>
2021-01-22 00:29:30 +0000

    [view-source] reduce unneeded code

--- youtube_dl/extractor/viewsource.py
+++ youtube_dl/extractor/viewsource.py
@@ -2,16 +2,14 @@
 from __future__ import unicode_literals
 
 from .common import InfoExtractor
+import re
 
 
 class ViewSourceIE(InfoExtractor):
     IE_DESC = False  # Do not list
-    IE_NAME = 'view-source'
     _VALID_URL = r'^view-source:'
-    _TEST = {}
 
     def _real_extract(self, url):
-        if url.startswith('view-source:'):
-            self._downloader.report_warning('URL is pasted with "view-source:" appended')
-            url = url[url.index(':') + 1:]
+        self._downloader.report_warning('URL is pasted with "view-source:" appended')
+        url = re.sub(self._VALID_URL, '', url)
         return self.url_result(url)
