f450afb0475175e335a4dc354d3ec29d68b593f9
nao20010128nao <nao20010128@gmail.com>
2021-01-22 00:19:41 +0000

    [view-source] fix behavior

--- youtube_dl/extractor/viewsource.py
+++ youtube_dl/extractor/viewsource.py
@@ -2,20 +2,16 @@
 from __future__ import unicode_literals
 
 from .common import InfoExtractor
-from ..utils import (
-    ExtractorError,
-)
 
 
 class ViewSourceIE(InfoExtractor):
     IE_DESC = False  # Do not list
     IE_NAME = 'view-source'
-    _VALID_URL = r'view-source:.*'
+    _VALID_URL = r'^view-source:'
     _TEST = {}
 
     def _real_extract(self, url):
         if url.startswith('view-source:'):
-            # remove "view-source:"
-            return self.url_result(url[:url.index(':')])
-        else:
-            raise ExtractorError('Not a view-source: URL', expected=True)
+            self._downloader.report_warning('URL is pasted with "view-source:" appended')
+            url = url[url.index(':') + 1:]
+        return self.url_result(url)
