bb6b64c5e37a2881d123ef540cc2aadb1cd97d3b
nao20010128nao <nao20010128@gmail.com>
2021-03-31 17:08:41 +0900

    [termux/youtubedl.sh] fix shellcheck errors

--- termux/youtubedl.sh
+++ termux/youtubedl.sh
@@ -1,4 +1,5 @@
 #!/system/bin/sh
+# shellcheck disable=SC2034,SC2039
 
 # Colors
 # ----------------------------------------
@@ -21,79 +22,79 @@ clear
 
 
 # see https://ideone.com/OxtAVZ
-echo -e $R"        _      _ _                    _       _              _ "$N
+echo -e "$R"'        _      _ _                    _       _              _ '"$N"
 sleep 0.3
-echo -e $R"       | |    | | |                  | |     | |            | |"$N
+echo -e "$R"'       | |    | | |                  | |     | |            | |'"$N"
 sleep 0.3
-echo -e $R"  _   _| |_ __| | |______ _ __   __ _| |_ ___| |__   ___  __| |"$N
+echo -e "$R"'  _   _| |_ __| | |______ _ __   __ _| |_ ___| |__   ___  __| |'"$N"
 sleep 0.3
-echo -e $R" | | | | __/ _` | |______| '_ \ / _` | __/ __| '_ \ / _ \/ _` |"$N
+echo -e "$R"' | | | | __/ _` | |______| '\''_ \ / _` | __/ __| '\''_ \ / _ \/ _` |'"$N"
 sleep 0.3
-echo -e $R" | |_| | || (_| | |      | |_) | (_| | || (__| | | |  __/ (_| |"$N
+echo -e "$R"' | |_| | || (_| | |      | |_) | (_| | || (__| | | |  __/ (_| |'"$N"
 sleep 0.3
-echo -e $R"  \__, |\__\__,_|_|      | .__/ \__,_|\__\___|_| |_|\___|\__,_|"$N
+echo -e "$R"'  \__, |\__\__,_|_|      | .__/ \__,_|\__\___|_| |_|\___|\__,_|'"$N"
 sleep 0.3
-echo -e $R"   __/ |                 | |                                   "$N
+echo -e "$R"'   __/ |                 | |                                   '"$N"
 sleep 0.3
-echo -e $R"  |___/                  |_|                                   "$N
+echo -e "$R"'  |___/                  |_|                                   '"$N"
 sleep 0.3
 
 sleep 1.5
 
-echo -e $Y $L"ytdl-patched Installer By"  $R "nao20010128nao" $N
+echo -e "$Y" "$L""ytdl-patched Installer By"  "$R" "nao20010128nao" "$N"
 
-echo -e $Y $L"Please accept permission access..." $N
+echo -e "$Y" "$L""Please accept permission access..." "$N"
 
 		termux-setup-storage
 		clear
 
-echo -e $Y $L"Installing python..." $N
+echo -e "$Y" "$L""Installing python..." "$N"
 
 		pkg install python -y
 		clear
 
-echo -e $Y $L"Installing ffmpeg..." $N
+echo -e "$Y" "$L""Installing ffmpeg..." "$N"
 
 		apt install ffmpeg -y
 		apt update -y
 		clear
 
-echo -e $Y $L"Installing wget..." $N
+echo -e "$Y" "$L""Installing wget..." "$N"
 
 		pkg install wget -y
 
-echo -e $Y $L "Creating bin folder..." $N
+echo -e "$Y" "$L" "Creating bin folder..." "$N"
 		mkdir ~/bin
 sleep 1.5
 
-echo -e $Y $L"Installing ytdl-patched..." $N 
+echo -e "$Y" "$L""Installing ytdl-patched..." "$N" 
 
 		wget https://github.com/nao20010128nao/ytdl-patched/releases/download/1617160331/youtube-dl -O /data/data/com.termux/files/home/bin/ytdl-patched
 		chmod a+x /data/data/com.termux/files/home/bin/ytdl-patched
 		/data/data/com.termux/files/home/bin/ytdl-patched -U
 		clear
 
-echo -e $Y $L"Setting up configs..." $N
+echo -e "$Y" "$L""Setting up configs..." "$N"
 sleep 2
 
-echo -e $Y $L "Creating Youtube folder..." $N
+echo -e "$Y" "$L" "Creating Youtube folder..." "$N"
 		mkdir /data/data/com.termux/files/home/storage/shared/Youtube
 sleep 1.5
 
-echo -e $Y $L "Creating youtube-dl config..." $N
+echo -e "$Y" "$L" "Creating youtube-dl config..." "$N"
 		mkdir -p ~/.config/youtube-dl
 sleep 1.5
 
-echo -e $Y $L "Getting config file..." $N
+echo -e "$Y" "$L" "Getting config file..." "$N"
 		wget https://raw.githubusercontent.com/nao20010128nao/ytdl-patched/master/termux/config -P /data/data/com.termux/files/home/.config/youtube-dl
 
-echo -e $Y $L "Getting files..." $N
+echo -e "$Y" "$L" "Getting files..." "$N"
 		wget https://raw.githubusercontent.com/nao20010128nao/ytdl-patched/master/termux/termux-url-opener -P /data/data/com.termux/files/home/bin
 		clear
 		
-echo -e $Y $L "Finishing configuration..." $N
+echo -e "$Y" "$L" "Finishing configuration..." "$N"
 sleep 5
 
-echo -e $G"Installation Finished..." $N
+echo -e "$G""Installation Finished..." "$N"
 
 kill -1 $PPID
