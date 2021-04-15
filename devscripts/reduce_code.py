16348678155c1e7b482b6d0a86aae9d1b6306b01
nao20010128nao <nao20010128@gmail.com>
2021-04-15 12:34:31 +0900

    [reduce_code] add regex cleanup

--- devscripts/reduce_code.py
+++ devscripts/reduce_code.py
@@ -3,9 +3,11 @@ from __future__ import unicode_literals
 
 # This script does the following using AST
 # - removes test cases from InfoExtractor
+# - clean up regexes when inline flag (?x) is set
 
 import ast
 import sys
+import re
 
 paths = sys.argv[1:]
 
@@ -27,6 +29,26 @@ for path in paths:
             if not isinstance(member, ast.Assign):
                 continue
             assign_name = member.targets[0].id
+            assign_value = member.value
+            # TODO: support formatted string like YoutubeIE
+            if assign_name == '_VALID_URL' and isinstance(assign_value, ast.Constant) and isinstance(assign_value.value, str):
+                # clean up regexes
+                regex_str = assign_value.value
+                has_extended = re.search(r'\(\?[aiLmsux]*x[aiLmsux]*\)', regex_str)  # something like (?xxs) may match, but (?s) or (?i) won't
+                if not has_extended:
+                    continue
+                # remove comments
+                regex_str = re.sub(r'(?m)\s*#.+?$', '', regex_str)
+                # remove spaces and indents
+                regex_str = re.sub(r'\s+', '', regex_str)
+                # remove x (EXTENDED) from all inline flags
+                regex_str = re.sub(r'\(\?([aiLmsux]+)\)', lambda m: '(?%s)' % m.group(1).replace('x', ''), regex_str)
+                regex_str = re.sub(r'\(\?\)', '', regex_str)
+                # set it back, if it is smaller
+                if len(assign_value.value) > len(regex_str):
+                    print('    Cleaning up _VALID_URL in %s' % stmt.name)
+                    assign_value.value = regex_str
+                    modified = True
             if assign_name not in denylist:
                 continue
             print('    Removing %s in %s' % (assign_name, stmt.name))
