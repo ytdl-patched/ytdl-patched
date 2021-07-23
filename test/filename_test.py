d78be4b45df675a56519b08539642cef0a7309fd
nao20010128nao <nao20010128@gmail.com>
2021-07-24 01:26:17 +0900

    [test] make filename_test.py import-safe

--- test/filename_test.py
+++ test/filename_test.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from __future__ import unicode_literals
 
-import sys
+if __name__ == '__main__':
+    import sys
 
-filename = sys.argv[1]
+    filename = sys.argv[1]
 
-sys.exit(0 if "it's a small world" in filename.lower() else 1)
+    sys.exit(0 if "it's a small world" in filename.lower() else 1)
