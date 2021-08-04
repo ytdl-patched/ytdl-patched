dd4aea3fa32ebc8dd1b9b8440801a6a1c6c6963d
nao20010128nao <nao20010128@gmail.com>
2021-08-04 14:08:41 +0900

    [build] install upx in alternative way

---
+++ devscripts/install_upx.ps1
@@ -0,0 +1,11 @@
+# UPX installer for GitHub Actions' Windows
+choco install upx
+
+if ($LASTEXITCODE -ne 0) {
+    # fallback to ZIP download
+    $UpxVersion = "3.96"
+    Invoke-WebRequest "https://github.com/upx/upx/releases/download/v${UpxVersion}/upx-${UpxVersion}-win64.zip" -O upx.zip
+    Expand-Archive -Path 'upx.zip' -DestinationPath 'upx\'
+    # add to PATH as we do for Actions
+    echo "$((Get-Item .).FullName)/upx-${UpxVersion}-win64" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+}
