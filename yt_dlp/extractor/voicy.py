98edd1bd4988038c18101d59ae45005d75a5b8d7
nao20010128nao <nao20010128@gmail.com>
2021-08-04 13:34:08 +0900

    [voicy] refactor and make it blazing fast

--- yt_dlp/extractor/voicy.py
+++ yt_dlp/extractor/voicy.py
@@ -19,7 +19,7 @@ class VoicyBaseIE(InfoExtractor):
     def _extract_from_playlist_data(self, value):
         voice_id = compat_str(value['PlaylistId'])
         upload_date = datetime.strptime(value['Published'], "%Y-%m-%dT%H:%M:%SZ").strftime('%Y%m%d')
-        items = [self._extract_single_article(voice_id, voice_data, index) for index, voice_data in enumerate(value['VoiceData'], start=1)]
+        items = [self._extract_single_article(voice_data) for voice_data in value['VoiceData']]
         result = self.playlist_result(items)
         result.update({
             'id': voice_id,
@@ -33,17 +33,19 @@ class VoicyBaseIE(InfoExtractor):
         return result
 
     # NOTE: "article" in voicy = "track" in CDs = "chapter" in DVDs
-    def _extract_single_article(self, voice_id, entry, index=None):
-        formats = self._extract_m3u8_formats(
-            entry['VoiceHlsFile'], voice_id, ext='m4a', entry_protocol='m3u8_native',
-            m3u8_id='hls', note=None if index is None else 'Downloading information for track %d' % index)
-        formats.append({
+    def _extract_single_article(self, entry):
+        formats = [{
+            'url': entry['VoiceHlsFile'],
+            'format_id': 'hls',
+            'ext': 'm4a',
+            'acodec': None,
+            'protocol': 'm3u8_native',
+        }, {
             'url': entry['VoiceFile'],
             'format_id': 'mp3',
             'ext': 'mp3',
-            'vcodec': 'none',
             'acodec': 'mp3',
-        })
+        }]
         self._sort_formats(formats)
         return {
             'id': compat_str(entry['ArticleId']),
@@ -134,11 +136,11 @@ class VoicyChannelIE(VoicyBaseIE):
 
         urls = [smuggle_url('https://voicy.jp/channel/%s/%d' % (channel_id, value['PlaylistId']), value) for value in articles]
         playlist = [self.url_result(url_, VoicyIE.ie_key()) for url_ in urls]
-        result = self.playlist_result(playlist)
-        result.update({
+        return {
+            '_type': 'playlist',
+            'entries': playlist,
             'id': channel_id,
             'title': title,
             'channel': channel_id,
             'channel_id': channel_id,
-        })
-        return result
+        }
