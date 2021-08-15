14af1c14e6bd95477bf53f59908f0df4a241fc1f
nao20010128nao <nao20010128@gmail.com>
2021-08-16 00:24:24 +0900

    [voicy] port changes back

--- yt_dlp/extractor/voicy.py
+++ yt_dlp/extractor/voicy.py
@@ -11,33 +11,33 @@ from ..utils import (
     unified_strdate,
 )
 
+import re
 import itertools
 
 
 class VoicyBaseIE(InfoExtractor):
-    # every queries are assumed to be a playlist
     def _extract_from_playlist_data(self, value):
-        voice_id = compat_str(value['PlaylistId'])
-        upload_date = unified_strdate(value['Published'], False)
+        voice_id = compat_str(value.get('PlaylistId'))
+        upload_date = unified_strdate(value.get('Published'), False)
         items = [self._extract_single_article(voice_data) for voice_data in value['VoiceData']]
         return {
-            '_type': 'playlist',
+            '_type': 'multi_video',
             'entries': items,
             'id': voice_id,
-            'title': compat_str(value['PlaylistName']),
-            'uploader': value['SpeakerName'],
-            'uploader_id': compat_str(value['SpeakerId']),
-            'channel': value['ChannelName'],
-            'channel_id': compat_str(value['ChannelId']),
+            'title': compat_str(value.get('PlaylistName')),
+            'uploader': value.get('SpeakerName'),
+            'uploader_id': compat_str(value.get('SpeakerId')),
+            'channel': value.get('ChannelName'),
+            'channel_id': compat_str(value.get('ChannelId')),
             'upload_date': upload_date,
         }
 
-    # NOTE: "article" in voicy = "track" in CDs = "chapter" in DVDs
     def _extract_single_article(self, entry):
         formats = [{
             'url': entry['VoiceHlsFile'],
             'format_id': 'hls',
             'ext': 'm4a',
+            'acodec': 'aac',
             'vcodec': 'none',
             'protocol': 'm3u8_native',
         }, {
@@ -49,22 +49,22 @@ class VoicyBaseIE(InfoExtractor):
         }]
         self._sort_formats(formats)
         return {
-            'id': compat_str(entry['ArticleId']),
-            'title': entry['ArticleTitle'],
-            'description': entry['MediaName'],
-            'voice_id': compat_str(entry['VoiceId']),
-            'chapter_id': compat_str(entry['ChapterId']),
+            'id': compat_str(entry.get('ArticleId')),
+            'title': entry.get('ArticleTitle'),
+            'description': entry.get('MediaName'),
+            'voice_id': compat_str(entry.get('VoiceId')),
+            'chapter_id': compat_str(entry.get('ChapterId')),
             'formats': formats,
         }
 
     def _call_api(self, url, video_id, **kwargs):
         response = self._download_json(url, video_id, **kwargs)
-        if response['Status'] != 0:
+        if response.get('Status') != 0:
             message = traverse_obj(response, ('Value', 'Error', 'Message'), expected_type=compat_str)
             if not message:
-                message = 'There was a error in the response: %d' % response['Status']
+                message = 'There was a error in the response: %d' % response.get('Status')
             raise ExtractorError(message, expected=False)
-        return response['Value']
+        return response.get('Value')
 
 
 class VoicyIE(VoicyBaseIE):
@@ -83,10 +83,11 @@ class VoicyIE(VoicyBaseIE):
         'playlist_mincount': 9,
     }]
 
-    # every queries are assumed to be a playlist
     def _real_extract(self, url):
-        voice_id = self._match_id(url)
-        channel_id = self._VALID_URL_RE.match(url).group('channel_id')
+        mobj = re.match(self._VALID_URL, url)
+        assert mobj
+        voice_id = mobj.group('id')
+        channel_id = mobj.group('channel_id')
         url, article_list = unsmuggle_url(url)
         if not article_list:
             article_list = self._call_api(self.ARTICLE_LIST_API_URL % (channel_id, voice_id), voice_id)
@@ -113,35 +114,39 @@ class VoicyChannelIE(VoicyBaseIE):
     def suitable(cls, url):
         return not VoicyIE.suitable(url) and super(VoicyChannelIE, cls).suitable(url)
 
-    def _real_extract(self, url):
-        channel_id = self._match_id(url)
-        articles = []
+    def _entries(self, channel_id):
         pager = ''
         for count in itertools.count(1):
             article_list = self._call_api(self.PROGRAM_LIST_API_URL % (channel_id, pager), channel_id, note='Paging #%d' % count)
-            playlist_data = article_list['PlaylistData']
+            playlist_data = article_list.get('PlaylistData')
             if not playlist_data:
                 break
-            articles.extend(playlist_data)
+            yield from playlist_data
             last = playlist_data[-1]
             pager = '&pid=%d&p_date=%s&play_count=%s' % (last['PlaylistId'], last['Published'], last['PlayCount'])
 
-        title = traverse_obj(articles, (0, 'ChannelName'), expected_type=compat_str)
-        if not title:
-            spaker_name = traverse_obj(articles, (0, 'SpeakerName'), expected_type=compat_str)
-            if spaker_name:
-                title = 'Uploads from %s' % spaker_name
+    def _real_extract(self, url):
+        channel_id = self._match_id(url)
+        articles = self._entries(channel_id)
+
+        first_article = next(articles, None)
+        title = traverse_obj(first_article, ('ChannelName', ), expected_type=compat_str)
+        speaker_name = traverse_obj(first_article, ('SpeakerName', ), expected_type=compat_str)
+        if not title and speaker_name:
+            title = 'Uploads from %s' % speaker_name
         if not title:
             title = 'Uploads from channel ID %s' % channel_id
 
-        playlist = [
+        articles = itertools.chain([first_article], articles) if first_article else articles
+
+        playlist = (
             self.url_result(smuggle_url('https://voicy.jp/channel/%s/%d' % (channel_id, value['PlaylistId']), value), VoicyIE.ie_key())
-            for value in articles]
+            for value in articles)
         return {
             '_type': 'playlist',
             'entries': playlist,
             'id': channel_id,
             'title': title,
-            'channel': channel_id,
+            'channel': speaker_name,
             'channel_id': channel_id,
         }
