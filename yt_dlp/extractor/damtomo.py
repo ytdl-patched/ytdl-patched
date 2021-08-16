98e822acfc474ceabe8231564a105be8c682a2c4
nao20010128nao <nao20010128@gmail.com>
2021-08-16 23:23:04 +0900

    [damtomo] clean up

--- yt_dlp/extractor/damtomo.py
+++ yt_dlp/extractor/damtomo.py
@@ -5,7 +5,7 @@ import re
 
 from .common import InfoExtractor
 from ..utils import ExtractorError, clean_html, int_or_none, try_get
-from ..compat import compat_etree_fromstring, compat_str
+from ..compat import compat_str
 
 
 class DamtomoIE(InfoExtractor):
@@ -48,28 +48,26 @@ class DamtomoIE(InfoExtractor):
         data_dict['user_name'] = re.sub(r'\s*さん\s*$', '', data_dict['user_name'])
         title = '%(song_title)s-%(song_artist)s-%(user_name)s' % data_dict
 
-        stream_xml = self._download_webpage(
+        stream_tree = self._download_xml(
             'https://www.clubdam.com/app/damtomo/karaokeMovie/GetStreamingDkmUrlXML.do?movieSelectFlg=2&karaokeMovieId=%s' % video_id, video_id,
             note='Requesting stream information', encoding='sjis')
-
-        stream_tree = compat_etree_fromstring(stream_xml)
         m3u8_url = try_get(stream_tree, lambda x: x.find(
             './/d:streamingUrl',
             {'d': 'https://www.clubdam.com/app/damtomo/karaokeMovie/GetStreamingDkmUrlXML'}).text.strip(), compat_str)
-        if not isinstance(m3u8_url, compat_str):
-            raise ExtractorError('Failed to obtain streaming URL')
-
-        formats = self._extract_m3u8_formats(
-            m3u8_url, video_id,
-            ext='mp4', entry_protocol='m3u8_native', m3u8_id='hls')
-        self._sort_formats(formats)
+        if not m3u8_url:
+            raise ExtractorError('Failed to obtain m3u8 URL')
 
         return {
             'id': video_id,
             'title': title,
             'uploader_id': uploader_id,
             'description': description,
-            'formats': formats,
+            'formats': [{
+                'format_id': 'hls',
+                'url': m3u8_url,
+                'ext': 'mp4',
+                'protocol': 'm3u8_native',
+            }],
             'uploader': data_dict['user_name'],
             'upload_date': try_get(data_dict, lambda x: self._search_regex(r'(\d\d\d\d/\d\d/\d\d)', x['date'], 'upload_date', default=None).replace('/', ''), compat_str),
             'view_count': int_or_none(self._search_regex(r'(\d+)', data_dict['audience'], 'view_count', default=None)),
