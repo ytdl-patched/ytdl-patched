1df02dc4702eaa89f20d923d9ebdf1680e3ff73b
nao20010128nao <nao20010128@gmail.com>
2021-08-16 20:03:19 +0900

    [twitcasting] enforce HLS download for m3u8 formats

--- yt_dlp/extractor/twitcasting.py
+++ yt_dlp/extractor/twitcasting.py
@@ -128,7 +128,8 @@ class TwitCastingIE(TwitCastingBaseIE):
             # use `m3u8` entry_protocol until EXT-X-MAP is properly supported by `m3u8_native` entry_protocol
             m3u8_url = m3u8_urls[0]
             formats = self._extract_m3u8_formats(
-                m3u8_url, video_id, 'mp4', m3u8_id='hls',
+                m3u8_url, video_id, ext='mp4', m3u8_id='hls',
+                entry_protocol='m3u8', is_live=True,
                 headers={
                     'Accept': '*/*',
                     'Origin': 'https://twitcasting.tv',
