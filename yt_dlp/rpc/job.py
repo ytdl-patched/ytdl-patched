cdc8bf3270cd10ef5c554699df5a3bffee914a8a
nao20010128nao <nao20010128@gmail.com>
2021-08-31 13:16:26 +0900

    [rpc] implement HTTP RPC server partially

--- yt_dlp/rpc/job.py
+++ yt_dlp/rpc/job.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import Dict, List, Type, Union
 
 
 JSONRPC_ID_KEY = Union[float, int, str, None]
@@ -40,3 +40,24 @@ class GetArgsJob(JobBase):
     """
     Responds with current presistent argument value as of the job is handled.
     """
+
+
+_REQTYPE_TO_CLASS: Dict[str, Type[JobBase]] = {
+    'arguments': ArgumentsJob,
+    'appendArgs': AppendArgsJob,
+    'resetArgs': ResetArgsJob,
+    'clearArgs': ClearArgsJob,
+    'getArgs': GetArgsJob,
+}
+
+
+def create_job_from_json(obj):
+    job_id = obj.get('id')
+    content = obj.get('data')
+    if not isinstance(content, dict):
+        return None
+    reqtype = content.get('type')
+    args = content.get('args') or {}
+    job = _REQTYPE_TO_CLASS[reqtype](**args)
+    job.job_id = job_id
+    return job
--- yt_dlp/rpc/server.py
+++ yt_dlp/rpc/server.py
@@ -1,8 +1,14 @@
+import json
+import base64
+
 from queue import Empty, Queue
-from typing import List
-from .job import JSONRPC_ID_KEY, AppendArgsJob, ArgumentsJob, ClearArgsJob, GetArgsJob, JobBase, ResetArgsJob
+from typing import Dict, List, Optional
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from .job import JSONRPC_ID_KEY, AppendArgsJob, ArgumentsJob, ClearArgsJob, GetArgsJob, JobBase, ResetArgsJob, create_job_from_json
 from .. import _read_ytdl_opts
 from ..utils import ExistingVideoReached, MaxDownloadsReached, RejectedVideoReached
+from ..compat import compat_urllib_parse_urlparse
+from ..version import __version__
 from ..YoutubeDL import YoutubeDL
 
 _TAIL_OF_QUEUE = object()
@@ -18,6 +24,110 @@ class RpcServerBase():
 
     def stop_server(self):
         pass
+
+
+class HttpRpcServer(RpcServerBase):
+
+    def __init__(self, listen: str, users: Dict[str, str] = None) -> None:
+        self.listen = listen
+        self.users = users
+        self.server: Optional[HTTPServer] = None
+
+    def start_server(self, job_queue: Queue[JobBase]):
+        hrs = self
+
+        class Handler(BaseHTTPRequestHandler):
+            def __init__(self, *args):
+                BaseHTTPRequestHandler.__init__(self, *args)
+
+            def do_GET(self):
+                parsed_path = compat_urllib_parse_urlparse(self.path)
+                if parsed_path.path == '/version':
+                    self.send_response(200)
+                    self.send_header('Content-type', 'application/json')
+                    self.end_headers()
+                    self.wfile.write(json.dumps({
+                        'brand': 'ytdl-patched',
+                        'version': __version__,
+                    }).encode('utf-8'))
+                else:
+                    # respond with 418 I'm a teapot
+                    self.send_response(418)
+                    self.end_headers()
+                return
+
+            def handle_auth(self):
+                if not hrs.users:
+                    # pass auth since no account is set
+                    return True
+                auth_header = self.headers.get('Authorization')
+                if not auth_header:
+                    return False
+                auth_header = str(auth_header)
+                if not auth_header.startswith('Basic '):
+                    return False
+                req_u, req_p = base64.b64decode(auth_header).decode('utf-8').split(':')
+                okay = False
+                for user, pwd in hrs.users.items():
+                    if user == req_u and pwd == req_p:
+                        okay = True
+                return okay
+
+            def do_POST(self):
+                if not self.handle_auth():
+                    self.send_response(403)
+                    self.end_headers()
+                    self.wfile.write(b'{"error":"Authentication failed."}')
+                    return
+
+                parsed_path = compat_urllib_parse_urlparse(self.path)
+                if parsed_path.path == '/enqueue':
+                    if self.headers.get('Content-Type') != 'application/json':
+                        self.send_response(403)
+                        self.end_headers()
+                        self.wfile.write(b'{"error":"You must upload JSON for POST payload."}')
+                        return
+                    post_data = json.load(self.rfile)
+                    job = create_job_from_json(post_data)
+                    job_queue.put(job)
+                    self.send_response(200)
+                    self.end_headers()
+                    self.wfile.write(b'{"response":"Enqueued"}')
+                elif parsed_path.path == '/shutdown':
+                    job_queue.put(_TAIL_OF_QUEUE)
+                    self.send_response(200)
+                    self.end_headers()
+                    self.wfile.write(b'{"response":"Goodbye"}')
+                    hrs.stop_server()
+                else:
+                    self.send_response(403)
+                    self.end_headers()
+                    self.wfile.write(b'{"error":"Unknown path."}')
+                    return
+
+        try:
+            from http.server import ThreadingHTTPServer
+            server = ThreadingHTTPServer((), Handler)
+        except ImportError:
+            server = HTTPServer((), Handler)
+        self.server = server
+        self.queue = job_queue
+        server.serve_forever()
+
+    def respond(self, response_id: JSONRPC_ID_KEY, data):
+        pass
+
+    def stop_server(self):
+        if self.server:
+            self.server.shutdown()
+            queue = getattr(self, 'queue', None)
+            if isinstance(queue, Queue):
+                queue.put(_TAIL_OF_QUEUE)
+            try:
+                delattr(self, 'queue')
+            except AttributeError:
+                pass
+        self.server = None
 
 
 def _queue_to_iter(q: Queue[JobBase]):
