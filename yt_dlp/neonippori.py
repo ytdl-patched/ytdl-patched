65a64556a388e26fa8a320c95a2489c6ea687b16
nao20010128nao <nao20010128@gmail.com>
2021-10-05 12:39:53 +0900

    [neonippori] strip code down

--- yt_dlp/neonippori.py
+++ yt_dlp/neonippori.py
@@ -11,7 +11,6 @@ import re
 import xml.dom.minidom
 import xml.etree.ElementTree as ET
 from .version import __version__
-from typing import Dict, Tuple
 
 
 def noop(*a, **b):
@@ -44,6 +43,24 @@ NICONICO_COLOR_MAPPINGS = {
     'purple2': 0x6633cc}
 
 
+def process_mailstyle(mail, fontsize):
+    pos = 0
+    color = 0xffffff
+    size = fontsize
+    for mailstyle in str(mail).split():
+        if mailstyle == 'ue':
+            pos = 1
+        elif mailstyle == 'shita':
+            pos = 2
+        elif mailstyle == 'big':
+            size = fontsize * 1.44
+        elif mailstyle == 'small':
+            size = fontsize * 0.64
+        elif mailstyle in NICONICO_COLOR_MAPPINGS:
+            color = NICONICO_COLOR_MAPPINGS[mailstyle]
+    return pos, color, size
+
+
 def parse_comments_nnxml(f: str, fontsize: float, report_warning):
     """ (timeline, timestamp, no, comment, pos, color, size, height, width) """
     dom = xml.dom.minidom.parse(f)
@@ -53,23 +70,10 @@ def parse_comments_nnxml(f: str, fontsize: float, report_warning):
             c = str(comment.childNodes[0].wholeText)
             if c.startswith('/'):
                 continue  # ignore advanced comments
-            pos = 0
-            color = 0xffffff
-            size = fontsize
-            for mailstyle in str(comment.getAttribute('mail')).split():
-                if mailstyle == 'ue':
-                    pos = 1
-                elif mailstyle == 'shita':
-                    pos = 2
-                elif mailstyle == 'big':
-                    size = fontsize * 1.44
-                elif mailstyle == 'small':
-                    size = fontsize * 0.64
-                elif mailstyle in NICONICO_COLOR_MAPPINGS:
-                    color = NICONICO_COLOR_MAPPINGS[mailstyle]
+            pos, color, size = process_mailstyle(comment.getAttribute('mail'), fontsize)
             yield (max(int(comment.getAttribute('vpos')), 0) * 0.01, int(comment.getAttribute('date')), int(comment.getAttribute('no')), c, pos, color, size, (c.count('\n') + 1) * size, maximum_line_length(c) * size)
-        except (AssertionError, AttributeError, IndexError, TypeError, ValueError):
-            report_warning('Invalid comment: %s' % comment.toxml())
+        except (AssertionError, AttributeError, IndexError, TypeError, ValueError) as e:
+            report_warning('Invalid comment: %s %s' % (e, comment.toxml()))
             continue
 
 
@@ -89,73 +93,13 @@ def parse_comments_nnjson(f: str, fontsize: float, report_warning):
                 continue
 
             c = comment['content']
+            if c.startswith('/'):
+                continue  # ignore advanced comments
 
-            pos = 0
-            color = 0xffffff
-            size = fontsize
-            for mailstyle in comment.get('mail', '').split():
-                if mailstyle == 'ue':
-                    pos = 1
-                elif mailstyle == 'shita':
-                    pos = 2
-                elif mailstyle == 'big':
-                    size = fontsize * 1.44
-                elif mailstyle == 'small':
-                    size = fontsize * 0.64
-                elif mailstyle in NICONICO_COLOR_MAPPINGS:
-                    color = NICONICO_COLOR_MAPPINGS[mailstyle]
-
+            pos, color, size = process_mailstyle(comment.get('mail', ''), fontsize)
             yield (max(comment['vpos'], 0) * 0.01, comment['date'], comment.get('no', 0), c, pos, color, size, (c.count('\n') + 1) * size, maximum_line_length(c) * size)
-        except (AssertionError, AttributeError, IndexError, TypeError, ValueError, KeyError):
-            report_warning('Invalid comment: %s' % (comment and json.dumps(comment)))
-            continue
-
-
-def parse_bilibili_comment_section(
-        comment, i, fontsize, time_scale,
-        min_length,
-        type_id, time_id, timestamp_id, color_id, size_id):
-
-    p = str(comment.getAttribute('p')).split(',')
-    assert len(p) >= min_length
-    assert p[type_id] in ('1', '4', '5', '6', '7', '8')
-    if comment.childNodes.length <= 0:
-        return
-    time = float(p[time_id]) / time_scale
-    if p[type_id] in ('1', '4', '5', '6'):
-        c = str(comment.childNodes[time_id].wholeText).replace('/n', '\n')
-        size = int(p[size_id]) * fontsize / 25.0
-        yield (time, int(p[timestamp_id]), i, c, '1456'.index(p[type_id]), int(p[color_id]), size, (c.count('\n') + 1) * size, maximum_line_length(c) * size)
-    elif p[type_id] == '7':  # positioned comment
-        c = str(comment.childNodes[time_id].wholeText)
-        yield (time, int(p[timestamp_id]), i, c, 'bilipos', int(p[color_id]), int(p[size_id]), 0, 0)
-    elif p[type_id] == '8':
-        return  # ignore scripted comment
-
-
-def parse_comments_bilibili(f: str, fontsize: float, report_warning):
-    dom = xml.dom.minidom.parse(f)
-    comment_element = dom.getElementsByTagName('d')
-    for i, comment in enumerate(comment_element):
-        try:
-            yield from parse_bilibili_comment_section(
-                comment, i, fontsize, 1, 5,
-                1, 0, 4, 3, 2)
-        except (AssertionError, AttributeError, IndexError, TypeError, ValueError):
-            report_warning('Invalid comment: %s' % comment.toxml())
-            continue
-
-
-def parse_comments_bilibili2(f: str, fontsize: float, report_warning):
-    dom = xml.dom.minidom.parse(f)
-    comment_element = dom.getElementsByTagName('d')
-    for i, comment in enumerate(comment_element):
-        try:
-            yield from parse_bilibili_comment_section(
-                comment, i, fontsize, 1000, 7,
-                3, 2, 6, 5, 4)
-        except (AssertionError, AttributeError, IndexError, TypeError, ValueError):
-            report_warning('Invalid comment: %s' % comment.toxml())
+        except (AssertionError, AttributeError, IndexError, TypeError, ValueError, KeyError) as e:
+            report_warning('Invalid comment: %s %s' % (e, comment and json.dumps(comment)))
             continue
 
 
@@ -202,159 +146,6 @@ def convert_niconico_json_to_xml(data: str) -> str:
     return '<?xml version="1.0" encoding="UTF-8"?>\n' + ET.tostring(packet, encoding='utf-8').decode('utf-8')
 
 
-PROCESSORS = {
-    'Niconico': parse_comments_nnxml,
-    'NiconicoJson': parse_comments_nnjson,
-    'Bilibili': parse_comments_bilibili,
-    'Bilibili2': parse_comments_bilibili2,
-}
-
-
-def write_danmaku_positioned_bilibili(f, c, width, height, styleid, player_size=(672, 438), report_warning=noop):
-    zoom_factor = calculate_zoom_factor(player_size, (width, height))
-
-    def get_position(input_pos, is_height):
-        is_height = int(is_height)  # True -> 1
-        if isinstance(input_pos, int):
-            return zoom_factor[0] * input_pos + zoom_factor[is_height + 1]
-        elif isinstance(input_pos, float):
-            if input_pos > 1:
-                return zoom_factor[0] * input_pos + zoom_factor[is_height + 1]
-            else:
-                return player_size[is_height] * zoom_factor[0] * input_pos + zoom_factor[is_height + 1]
-        else:
-            input_pos = float(input_pos)
-            return get_position(input_pos, is_height)
-
-    try:
-        comment_args = safe_list(json.loads(c[3]))
-        text = escape_ass_text(str(comment_args[4]).replace('/n', '\n'))
-        from_x = comment_args.get(0, 0)
-        from_y = comment_args.get(1, 0)
-        to_x = comment_args.get(7, from_x)
-        to_y = comment_args.get(8, from_y)
-        from_x = get_position(from_x, False)
-        from_y = get_position(from_y, True)
-        to_x = get_position(to_x, False)
-        to_y = get_position(to_y, True)
-        alpha = safe_list(str(comment_args.get(2, '1')).split('-'))
-        from_alpha = float(alpha.get(0, 1))
-        to_alpha = float(alpha.get(1, from_alpha))
-        from_alpha = 255 - round(from_alpha * 255)
-        to_alpha = 255 - round(to_alpha * 255)
-        rotate_z = int(comment_args.get(5, 0))
-        rotate_y = int(comment_args.get(6, 0))
-        lifetime = float(comment_args.get(3, 4500))
-        duration = int(comment_args.get(9, lifetime * 1000))
-        delay = int(comment_args.get(10, 0))
-        fontface = comment_args.get(12)
-        isborder = comment_args.get(11, 'true')
-        from_rotarg = convert_flash_rot(rotate_y, rotate_z, from_x, from_y, width, height)
-        to_rotarg = convert_flash_rot(rotate_y, rotate_z, to_x, to_y, width, height)
-        styles = ['\\org(%d, %d)' % (width / 2, height / 2)]
-        if from_rotarg[0:2] == to_rotarg[0:2]:
-            styles.append('\\pos(%.0f, %.0f)' % (from_rotarg[0:2]))
-        else:
-            styles.append('\\move(%.0f, %.0f, %.0f, %.0f, %.0f, %.0f)' % (from_rotarg[0:2] + to_rotarg[0:2] + (delay, delay + duration)))
-        styles.append('\\frx%.0f\\fry%.0f\\frz%.0f\\fscx%.0f\\fscy%.0f' % (from_rotarg[2:7]))
-        if (from_x, from_y) != (to_x, to_y):
-            styles.append('\\t(%d, %d, ' % (delay, delay + duration))
-            styles.append('\\frx%.0f\\fry%.0f\\frz%.0f\\fscx%.0f\\fscy%.0f' % (to_rotarg[2:7]))
-            styles.append(')')
-        if fontface:
-            styles.append('\\fn%s' % escape_ass_text(fontface))
-        styles.append('\\fs%.0f' % (c[6] * zoom_factor[0]))
-        if c[5] != 0xffffff:
-            styles.append('\\c&H%s&' % format_color(c[5]))
-            if c[5] == 0x000000:
-                styles.append('\\3c&HFFFFFF&')
-        if from_alpha == to_alpha:
-            styles.append('\\alpha&H%02X' % from_alpha)
-        elif (from_alpha, to_alpha) == (255, 0):
-            styles.append('\\fad(%.0f,0)' % (lifetime * 1000))
-        elif (from_alpha, to_alpha) == (0, 255):
-            styles.append('\\fad(0, %.0f)' % (lifetime * 1000))
-        else:
-            styles.append('\\fade(%(from_alpha)d, %(to_alpha)d, %(to_alpha)d, 0, %(end_time).0f, %(end_time).0f, %(end_time).0f)' % {'from_alpha': from_alpha, 'to_alpha': to_alpha, 'end_time': lifetime * 1000})
-        if isborder == 'false':
-            styles.append('\\bord0')
-        f.write('Dialogue: -1,%(start)s,%(end)s,%(styleid)s,,0,0,0,,{%(styles)s}%(text)s\n' % {'start': format_timestamp(c[0]), 'end': format_timestamp(c[0] + lifetime), 'styles': ''.join(styles), 'text': text, 'styleid': styleid})
-    except (IndexError, ValueError):
-        try:
-            report_warning('Invalid comment: %r' % c[3])
-        except IndexError:
-            report_warning('Invalid comment: %r' % c)
-
-
-def calculate_zoom_factor_raw(source: Tuple[float, float], target: Tuple[float, float]) -> Tuple[float, float, float]:
-    try:
-        source_ratio = source[0] / source[1]
-        target_ratio = target[0] / target[1]
-        if target_ratio < source_ratio:  # narrower
-            scale_factor = target[0] / source[0]
-            return scale_factor, 0, (target[1] - target[0] / (source_ratio * 2))
-        elif target_ratio > source_ratio:  # wider
-            scale_factor = target[1] / source[1]
-            return scale_factor, (target[0] - target[1] * source_ratio) / 2, 0
-        else:
-            return target[0] / source[0], 0, 0
-    except ZeroDivisionError:
-        return 1, 0, 0
-
-
-ZOOM_FACTOR_CACHE: Dict[
-    Tuple[Tuple[float, float], Tuple[float, float]],
-    Tuple[float, float, float]
-] = {}
-
-
-def calculate_zoom_factor(source, target):
-    k = (source, target)
-    if k not in ZOOM_FACTOR_CACHE:
-        ZOOM_FACTOR_CACHE[k] = calculate_zoom_factor_raw(source, target)
-    return ZOOM_FACTOR_CACHE[k]
-
-
-def cap_angle(deg):
-    return 180 - ((180 - deg) % 360)
-
-
-def convert_flash_rot(rotY, rotZ, X, Y, width, height, report_warning):
-    rotY = cap_angle(rotY)
-    rotZ = cap_angle(rotZ)
-    if rotY in (90, -90):
-        rotY -= 1
-    if rotY == 0 or rotZ == 0:
-        outX = 0
-        outY = -rotY  # Positive value means clockwise in Flash
-        outZ = -rotZ
-        rotY *= math.pi / 180.0
-        rotZ *= math.pi / 180.0
-    else:
-        rotY *= math.pi / 180.0
-        rotZ *= math.pi / 180.0
-        outY = math.atan2(-math.sin(rotY) * math.cos(rotZ), math.cos(rotY)) * 180 / math.pi
-        outZ = math.atan2(-math.cos(rotY) * math.sin(rotZ), math.cos(rotZ)) * 180 / math.pi
-        outX = math.asin(math.sin(rotY) * math.sin(rotZ)) * 180 / math.pi
-    trX = (X * math.cos(rotZ) + Y * math.sin(rotZ)) / math.cos(rotY) + (1 - math.cos(rotZ) / math.cos(rotY)) * width / 2 - math.sin(rotZ) / math.cos(rotY) * height / 2
-    trY = Y * math.cos(rotZ) - X * math.sin(rotZ) + math.sin(rotZ) * width / 2 + (1 - math.cos(rotZ)) * height / 2
-    trZ = (trX - width / 2) * math.sin(rotY)
-    FOV = width * math.tan(2 * math.pi / 9.0) / 2
-    try:
-        scaleXY = FOV / (FOV + trZ)
-    except ZeroDivisionError:
-        report_warning('Rotation makes object behind the camera: trZ == %.0f' % trZ)
-        scaleXY = 1
-    trX = (trX - width / 2) * scaleXY + width / 2
-    trY = (trY - height / 2) * scaleXY + height / 2
-    if scaleXY < 0:
-        scaleXY = -scaleXY
-        outX += 180
-        outY += 180
-        report_warning('Rotation makes object behind the camera: trZ == %.0f < %.0f' % (trZ, FOV))
-    return (trX, trY, cap_angle(outX), cap_angle(outY), cap_angle(outZ), scaleXY * 100, scaleXY * 100)
-
-
 def process_comments(comments, f, width, height, bottomReserved, fontface, fontsize, alpha, duration_marquee, duration_still, report_warning):
     styleid = 'NeoNippori_%04x' % random.randint(0, 0xffff)
     write_ass_header(f, width, height, fontface, fontsize, alpha, styleid)
@@ -375,8 +166,6 @@ def process_comments(comments, f, width, height, bottomReserved, fontface, fonts
                 row = find_alternative_row(rows, i, height, bottomReserved)
                 mark_comment_raw(rows, i, row)
                 write_comment(f, i, row, width, height, bottomReserved, fontsize, duration_marquee, duration_still, styleid)
-        elif i[4] == 'bilipos':
-            write_danmaku_positioned_bilibili(f, i, width, height, styleid, report_warning)
         else:
             report_warning('Invalid comment: %r' % i[3])
 
@@ -504,11 +293,7 @@ def format_timestamp(timestamp: float):
     return '%d:%02d:%02d.%02d' % (int(hour), int(minute), int(second), int(centsecond))
 
 
-def clip_byte(x):
-    return 255 if x > 255 else 0 if x < 0 else round(x)
-
-
-def format_color(color, width=1280, height=576):
+def format_color(color):
     if color == 0x000000:
         return '000000'
     elif color == 0xffffff:
@@ -516,14 +301,8 @@ def format_color(color, width=1280, height=576):
     R = (color >> 16) & 0xff
     G = (color >> 8) & 0xff
     B = color & 0xff
-    if width < 1280 and height < 576:
-        return '%02X%02X%02X' % (B, G, R)
-    else:  # VobSub always uses BT.601 colorspace, convert to BT.709
-        return '%02X%02X%02X' % (
-            clip_byte(R * 0.00956384088080656 + G * 0.03217254540203729 + B * 0.95826361371715607),
-            clip_byte(R * -0.10493933142075390 + G * 1.17231478191855154 + B * -0.06737545049779757),
-            clip_byte(R * 0.91348912373987645 + G * 0.07858536372532510 + B * 0.00792551253479842)
-        )
+
+    return '%02X%02X%02X' % (B, G, R)
 
 
 def convert_type2(row, height, bottomReserved):
@@ -534,12 +313,10 @@ def filter_badchars(f):
     return re.sub('[\\x00-\\x08\\x0b\\x0c\\x0e-\\x1f]', '\ufffd', f)
 
 
-class safe_list(list):
-    def get(self, index, default=None):
-        try:
-            return self[index]
-        except IndexError:
-            return default
+PROCESSORS = {
+    'Niconico': parse_comments_nnxml,
+    'NiconicoJson': parse_comments_nnjson,
+}
 
 
 def parse_comments(input_text, input_format, font_size=25.0, report_warning=noop):
