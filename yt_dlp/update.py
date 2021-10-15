eda31cf127547f19cf3466458173696a0b0c8a56
nao20010128nao <nao20010128@gmail.com>
2021-10-15 20:31:46 +0900

    [update] fix version comparison

--- yt_dlp/update.py
+++ yt_dlp/update.py
@@ -105,7 +105,7 @@ def run_update(ydl):
     def version_tuple(version_str):
         return tuple(map(int, version_str.split('.')))
 
-    version_id = version_info['tag_name']
+    version_id = version_info['name']
     if version_tuple(__version__) >= version_tuple(version_id):
         ydl.to_screen(f'ytdl-patched is up to date ({__version__})')
         return
