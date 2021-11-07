0441f821bab9dabe26769c314d1bd0c5c62df7ae
nao20010128nao <nao20010128@gmail.com>
2021-11-07 17:23:24 +0900

    [mixch] cleanup

--- yt_dlp/extractor/mixch.py
+++ yt_dlp/extractor/mixch.py
@@ -14,7 +14,19 @@ class MixchIE(InfoExtractor):
 
     TESTS = [{
         'url': 'https://mixch.tv/u/16137876/live',
-        'only_matching': True,
+        'skip': 'live has ended',
+    }, {
+        'url': 'https://mixch.tv/u/16236849/live',
+        'skip': 'don\'t know if this live persists',
+        'info_dict': {
+            'id': '16236849',
+            'title': '24配信シェア⭕️投票🙏💦',
+            'comment_count': 13145,
+            'view_count': 28348,
+            'timestamp': 1636189377,
+            'uploader': '🦥伊咲👶🏻#フレアワ',
+            'uploader_id': '16236849',
+        }
     }]
 
     def _real_extract(self, url):
@@ -27,29 +39,20 @@ class MixchIE(InfoExtractor):
         if not initial_js_state.get('liveInfo'):
             raise ExtractorError('Live has ended.', expected=True)
 
-        title = traverse_obj(initial_js_state, ('liveInfo', 'title'))
-        comment_count = traverse_obj(initial_js_state, ('liveInfo', 'comments'))
-        view_count = traverse_obj(initial_js_state, ('liveInfo', 'visitor'))
-        timestamp = traverse_obj(initial_js_state, ('liveInfo', 'created'))
-        uploader = traverse_obj(initial_js_state, ('broadcasterInfo', 'name'))
-
         # the service does not provide alternative resolutions
         hls_url = traverse_obj(initial_js_state, ('liveInfo', 'hls')) or 'https://d1hd0ww6piyb43.cloudfront.net/hls/torte_%s.m3u8' % video_id
+        formats = self._extract_m3u8_formats(
+            hls_url, video_id, ext='mp4', m3u8_id='hls')
 
         return {
             'id': video_id,
-            'title': title,
-            'comment_count': comment_count,
-            'view_count': view_count,
-            'timestamp': timestamp,
-            'uploader': uploader,
+            'title': traverse_obj(initial_js_state, ('liveInfo', 'title')),
+            'comment_count': traverse_obj(initial_js_state, ('liveInfo', 'comments')),
+            'view_count': traverse_obj(initial_js_state, ('liveInfo', 'visitor')),
+            'timestamp': traverse_obj(initial_js_state, ('liveInfo', 'created')),
+            'uploader': traverse_obj(initial_js_state, ('broadcasterInfo', 'name')),
             'uploader_id': video_id,
-            'formats': [{
-                'format_id': 'hls',
-                'url': hls_url,
-                'protocol': 'm3u8',
-                'ext': 'mp4',
-            }],
+            'formats': formats,
             'is_live': True,
             'webpage_url': url,
         }
