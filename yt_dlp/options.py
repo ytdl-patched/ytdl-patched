989fa1cb72824ece39e6d223b3302335e232277f
nao20010128nao <nao20010128@gmail.com>
2021-12-30 21:53:11 +0900

    [options] add `--playlist-index` as alias to `--playlist-items`

--- yt_dlp/options.py
+++ yt_dlp/options.py
@@ -430,7 +430,7 @@ def parseOpts(overrideArguments=None):
         dest='playlistend', metavar='NUMBER', default=None, type=int,
         help='Playlist video to end at (default is last)')
     selection.add_option(
-        '--playlist-items',
+        '--playlist-items', '--playlist-index',
         dest='playlist_items', metavar='ITEM_SPEC', default=None,
         help='Playlist video items to download. Specify indices of the videos in the playlist separated by commas like: "--playlist-items 1,2,5,8" if you want to download videos indexed 1, 2, 5, 8 in the playlist. You can specify range: "--playlist-items 1-3,7,10-13", it will download the videos at index 1, 2, 3, 7, 10, 11, 12 and 13')
     selection.add_option(
