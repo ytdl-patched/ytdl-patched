364732996eb49e717863236769d49218a89f0b12
Lesmiscore <nao20010128@gmail.com>
2022-01-06 00:23:59 +0900

    [YoutubeDL] allow to access original title even on livestreams

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -2499,14 +2499,14 @@ class YoutubeDL(object):
         if not self.params.get('allow_unplayable_formats'):
             formats = [f for f in formats if not f.get('has_drm')]
 
+        # backward compatibility, or for backing up original title modified below
+        info_dict['fulltitle'] = info_dict['title']
+
         if info_dict.get('is_live'):
             get_from_start = bool(self.params.get('live_from_start'))
             formats = [f for f in formats if bool(f.get('is_from_start')) == get_from_start]
             if not get_from_start:
                 info_dict['title'] += ' ' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-
-        # backward compatibility
-        info_dict['fulltitle'] = info_dict['title']
 
         if not formats:
             self.raise_no_formats(info_dict)
