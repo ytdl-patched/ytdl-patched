3ed7db158bbfc6ef5e2883b4a0c515c964ff0c82
Lesmiscore <nao20010128@gmail.com>
2022-01-16 23:54:25 +0900

    [disneychris] rewrite extractor

--- yt_dlp/extractor/disneychris.py
+++ yt_dlp/extractor/disneychris.py
@@ -4,58 +4,48 @@ import re
 
 from .common import InfoExtractor
 from ..utils import (
-    clean_html,
     determine_ext,
+    int_or_none,
 )
 
 
-class DisneyChrisBaseIE(InfoExtractor):
-    def extract_article_sections(self, webpage, base_id):
-        sections = []
-        for idx, x in enumerate(re.finditer(r'(?s)<td style="text-align: center;">(.+?)</td>', webpage)):
-            content = x.group(1)
-
-            title = self._search_regex(r'class="ch\d{2}Ttl[AB]">(.+?)\s*&nbsp;<span', content, 'title', group=1, default=False)
-            title = clean_html(title)
-
-            description = self._search_regex(r'class="ch\d{2}TrkC">(.+?)</div>', content, 'description', group=1, default=None)
-            description = clean_html(description)
-
-            audio_url = self._search_regex(r'class="amazingaudioplayer-source" data-src="(.+\.mp3)"', content, 'audio url', group=1, default=False)
-            if not title or not audio_url:
-                continue
-
-            title = re.sub(r'^\d+\s*-\s*', '', title)
-
-            sections.append({
-                'id': '%s-%d' % (base_id, idx),
-                'title': title,
-                'description': description,
-                'url': audio_url,
-                'ext': determine_ext(audio_url, 'mp3'),
-                'vcodec': 'none',
-                'acodec': 'mp3',
-            })
-
-        return sections
-
-
-class DisneyChrisIE(DisneyChrisBaseIE):
+class DisneyChrisIE(InfoExtractor):
     IE_NAME = 'disneychris'
     _VALID_URL = r'https?://(?:www\.)?disneychris\.com/(?P<id>a-day-at-disneyland|15-disneyland-soundtracks/.+?)\.html'
 
     def _real_extract(self, url):
         long_id = self._match_id(url)
-        video_id = long_id.split('/')[-1]
+        video_id = long_id.rsplit('/', 1)[-1]
         webpage = self._download_webpage(url, video_id)
-        sections = self.extract_article_sections(webpage, video_id)
 
-        title = None
         for section in re.finditer(r'href="/(.+?)\.html">(.+?)</a>', webpage):
             if section.group(1) == long_id:
                 title = section.group(2)
                 break
         else:
-            title = self._search_regex(r'<title>(.+?)</title>', webpage, 'playlist title', group=1)
+            title = self._html_extract_title(webpage, 'playlist title')
 
-        return self.playlist_result(sections, video_id, title)
+        return self.playlist_result(self.extract_article_sections(webpage, video_id), video_id, title)
+
+    def extract_article_sections(self, webpage, base_id):
+        for idx, x in enumerate(re.finditer(r'(?s)<td style="text-align: center;">(.+?)</td>', webpage)):
+            content = x.group(1)
+
+            title = self._html_search_regex(r'class="ch\d{2}Ttl[AB]">(.+?)\s*&nbsp;<span', content, 'title', fatal=False)
+
+            audio_url = self._search_regex(r'class="amazingaudioplayer-source" data-src="(.+\.mp3)"', content, 'audio url', fatal=False)
+            if not title or not audio_url:
+                continue
+
+            track_number, title = re.search(r'^(\d+)\s*-\s*(.+)', title).groups()
+
+            yield {
+                'id': '%s-%d' % (base_id, idx),
+                'title': title,
+                'description': self._html_search_regex(r'class="ch\d{2}TrkC">(.+?)</div>', content, 'description', fatal=False),
+                'url': audio_url,
+                'ext': determine_ext(audio_url, 'mp3'),
+                'vcodec': 'none',
+                'acodec': 'mp3',
+                'track_number': int_or_none(track_number),
+            }
