a7100a9f64d0adade1d3c9b65a1d93f152756018
Lesmiscore <nao20010128@gmail.com>
2022-01-30 23:52:56 +0900

    [bigo] inline `formats`

--- yt_dlp/extractor/bigo.py
+++ yt_dlp/extractor/bigo.py
@@ -7,9 +7,7 @@ from ..utils import ExtractorError
 
 class BigoIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?bigo\.tv/(?:[a-z]{2,}/)?(?P<id>[^/]+)'
-    # Note: I would like to provide some real test, but Bigo is a live streaming
-    # site, and a test would require that the broadcaster is live at the moment.
-    # So, I don't have a good way to provide a real test here.
+
     _TESTS = [{
         'url': 'https://www.bigo.tv/ja/221338632',
         'info_dict': {
@@ -44,19 +42,14 @@ class BigoIE(InfoExtractor):
         if not info.get('alive'):
             raise ExtractorError('This user is offline.', expected=True)
 
-        # formats = self._extract_m3u8_formats(
-        #     info.get('hls_src'), user_id, ext='mp4')
-        formats = [{
-            'url': info.get('hls_src'),
-            'ext': 'mp4',
-            'protocol': 'm3u8',
-            'is_live': True,
-        }]
-
         return {
             'id': info.get('roomId') or user_id,
             'title': info.get('roomTopic'),
-            'formats': formats,
+            'formats': [{
+                'url': info.get('hls_src'),
+                'ext': 'mp4',
+                'protocol': 'm3u8',
+            }],
             'thumbnail': info.get('snapshot'),
             'uploader': info.get('nick_name'),
             'uploader_id': user_id,
