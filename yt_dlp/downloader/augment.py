01c4acc0c90b2c9091ea967af31c37ad7ad051d6
Lesmiscore <nao20010128@gmail.com>
2022-03-31 02:28:39 +0900

    augment: implement HTTP server (WIP)

--- yt_dlp/downloader/augment.py
+++ yt_dlp/downloader/augment.py
@@ -1,7 +1,11 @@
 from __future__ import division, unicode_literals
 
+import functools
+import re
 import threading
 import typing
+import http.server
+import inspect
 
 if typing.TYPE_CHECKING:
     from .common import FileDownloader
@@ -120,4 +124,104 @@ class HeartbeatAugment(Augment):
             self.params['after_dl'](self)
 
 
-AUGMENT_MAP = {v._AUGMENT_KEY: v for v in (HeartbeatAugment, )}
+class HttpServerAugment(Augment):
+    """
+    Augment for intermediate HTTP server.
+
+    Keys:
+
+    before_dl: Callable to run before download starts. Arguments are: (HeartbeatAugment)
+               Can be used even if any of "callback", "url" and "data" are used.
+    after_dl: Callable to run after download ends. Arguments are: (HeartbeatAugment)
+    """
+    _AUGMENT_KEY = 'http_server'
+
+    def __init__(self, dl: 'FileDownloader', info_dict, params: dict) -> None:
+        super().__init__(dl, info_dict, params)
+        params, info_dict = self.params, self.info_dict
+
+    def start(self):
+        if 'before_dl' in self.params:
+            self.params['before_dl'](self)
+
+    def end(self):
+        if 'after_dl' in self.params:
+            self.params['after_dl'](self)
+
+    def create_handler_class(self, struct):
+        # struct = {
+        #     'routes': [{
+        #         'method': 'GET',
+        #         'route': '/hello/:world',
+        #         'callback': lambda handler: True
+        #     }, {
+        #         'method': 'GET',
+        #         'route': '/static/test',
+        #         'data': b'',
+        #     }]
+        # }
+
+        def respond_constant(value, status_code, headers, handler):
+            handler.send_response(status_code)
+            for k, v in headers.items():
+                handler.send_header(k, v)
+            handler.end_headers()
+            handler.wfile.write(value)
+            return True
+
+        def compile_route(route):
+            if route.startswith('re:'):
+                route = route[3:]
+            else:
+                route = re.escape(route)
+            return re.compile(route)
+
+        def process_route(regex, callback, handler):
+            mobj = re.fullmatch(regex, handler.path)
+            if not mobj:
+                return False
+            try:
+                setattr(handler, 'route_params', mobj.groupdict())
+                return callback(handler)
+            finally:
+                delattr(handler, 'route_params')
+
+        def chain(callbacks, handler):
+            for cb in callbacks:
+                if cb(handler):
+                    return True
+            return False
+
+        def wrapper(handler):
+            assert struct(handler)
+
+        if not inspect.isfunction(struct):
+            route_callbacks = []
+            for r in struct['routes']:
+                if inspect.isfunction(r):
+                    route_callbacks.append(r)
+                    continue
+                if 'data' in r:
+                    dd = r['data']
+                    if isinstance(dd, str):
+                        dd = dd.encode()
+                    r['callback'] = functools.partial(
+                        respond_constant, dd, r.get('status_code', 200), r.get('headers') or {})
+
+                route_callbacks.append(functools.partial(
+                    process_route, compile_route(r['route']), r['callback']))
+
+            struct = functools.partial(chain, route_callbacks)
+
+        return type('Lol this is allowed here', (ReqHandlerBase, ), {
+            'do_GET': wrapper,
+            'do_POST': wrapper,
+        })
+
+
+class ReqHandlerBase(http.server.BaseHTTPRequestHandler):
+    def log_message(self, format, *args):
+        pass
+
+
+AUGMENT_MAP = {v._AUGMENT_KEY: v for v in (HeartbeatAugment, HttpServerAugment)}
