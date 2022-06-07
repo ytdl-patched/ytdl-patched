b0369063d87acb568c76be4473fe57543a20a003
Lesmiscore <nao20010128@gmail.com>
2022-06-08 07:01:18 +0900

    downloader/websocket: reorganize classes

--- yt_dlp/downloader/websocket.py
+++ yt_dlp/downloader/websocket.py
@@ -9,34 +9,30 @@ from ..compat import asyncio
 from ..dependencies import websockets
 
 
-class FFmpegSinkFD(FileDownloader):
+class AsyncSinkFD(FileDownloader):
+    async def connect(self, stdin, info_dict):
+        try:
+            await self.real_connection(stdin, info_dict)
+        except OSError:
+            pass
+        finally:
+            with contextlib.suppress(OSError):
+                stdin.flush()
+                stdin.close()
+            os.kill(os.getpid(), signal.SIGINT)
+
+    async def real_connection(self, sink, info_dict):
+        """ Override this in subclasses """
+        raise NotImplementedError('This method must be implemented by subclasses')
+
+
+class FFmpegSinkFD(AsyncSinkFD):
     """ A sink to ffmpeg for downloading fragments in any form """
 
     def real_download(self, filename, info_dict):
         info_copy = info_dict.copy()
         info_copy['url'] = '-'
-
-        async def call_conn(proc, stdin):
-            try:
-                await self.real_connection(stdin, info_dict)
-            except OSError:
-                pass
-            finally:
-                with contextlib.suppress(OSError):
-                    stdin.flush()
-                    stdin.close()
-                os.kill(os.getpid(), signal.SIGINT)
-
-        if True:
-            try:
-                self.to_screen('Dumping raw data as output. Ctrl+C to stop.')
-                asyncio.run(call_conn(None, open(filename, 'wb')))
-            except KeyboardInterrupt:
-                pass
-            except OSError as e:
-                self.report_warning(e)
-            # prevent subsequent PPs to run; it's just a debug code!
-            return False
+        connect = self.connect
 
         class FFmpegStdinFD(FFmpegFD):
             @classmethod
@@ -44,14 +40,22 @@ class FFmpegSinkFD(FileDownloader):
                 return FFmpegFD.get_basename()
 
             def on_process_started(self, proc, stdin):
-                thread = threading.Thread(target=asyncio.run, daemon=True, args=(call_conn(proc, stdin), ))
+                thread = threading.Thread(target=asyncio.run, daemon=True, args=(connect(stdin, info_dict), ))
                 thread.start()
 
         return FFmpegStdinFD(self.ydl, self.params or {}).download(filename, info_copy)
 
-    async def real_connection(self, sink, info_dict):
-        """ Override this in subclasses """
-        raise NotImplementedError('This method must be implemented by subclasses')
+
+class FileSinkFD(AsyncSinkFD):
+    """ A sink to a file for downloading fragments in any form """
+    def real_download(self, filename, info_dict):
+        tempname = self.temp_name(filename)
+        try:
+            with open(tempname, 'wb') as w:
+                asyncio.run(self.connect(w, info_dict))
+        finally:
+            self.ydl.replace(tempname, filename)
+        return True
 
 
 class WebSocketFragmentFD(FFmpegSinkFD):
