d8e002fc4c545ccc6bcbd5e8eacbb29c78edc651
Lesmiscore <nao20010128@gmail.com>
2022-12-01 01:32:19 +0900

    fix `test_prepare_outtmpl_and_filename` test

--- test/test_YoutubeDL.py
+++ test/test_YoutubeDL.py
@@ -712,7 +712,11 @@ class TestYoutubeDL(unittest.TestCase):
         test('%(id)s', '-abcd', info={'id': '-abcd'})
         test('%(id)s', '.abcd', info={'id': '.abcd'})
         test('%(id)s', 'ab__cd', info={'id': 'ab__cd'})
-        test('%(id)s', ('ab:cd', 'ab -cd'), info={'id': 'ab:cd'})
+        if compat_os_name == 'nt':
+            test('%(id)s', ('ab:cd', 'ab -cd'), info={'id': 'ab:cd'})
+        else:
+            # change: non-Windows mode preserves colons
+            test('%(id)s', 'ab:cd', info={'id': 'ab:cd'})
         test('%(id.0)s', '-', info={'id': '--'})
 
         # Invalid templates
@@ -761,7 +765,11 @@ class TestYoutubeDL(unittest.TestCase):
         test('a%(width|)d', 'a', outtmpl_na_placeholder='none')
 
         FORMATS = self.outtmpl_info['formats']
-        sanitize = lambda x: x.replace(':', ' -').replace('"', "'").replace('\n', ' ')
+        if compat_os_name == 'nt':
+            sanitize = lambda x: x.replace(':', ' -').replace('"', "'").replace('\n', ' ')
+        else:
+            # change: non-Windows mode retains most of the characters in question
+            sanitize = lambda x: x.replace('\n', ' ')
 
         # Custom type casting
         test('%(formats.:.id)l', 'id 1, id 2, id 3')
@@ -779,13 +787,14 @@ class TestYoutubeDL(unittest.TestCase):
         test('%(filesize)#D', '1Ki')
         test('%(height)5.2D', ' 1.08k')
         test('%(title4)#S', 'foo_bar_test')
-        test('%(title4).10S', ('foo \'bar\' ', 'foo \'bar\'' + ('#' if compat_os_name == 'nt' else ' ')))
         if compat_os_name == 'nt':
+            test('%(title4).10S', ('foo \'bar\' ', 'foo \'bar\'#'))
             test('%(title4)q', ('"foo \\"bar\\" test"', "'foo _'bar_' test'"))
             test('%(formats.:.id)#q', ('"id 1" "id 2" "id 3"', "'id 1' 'id 2' 'id 3'"))
             test('%(formats.0.id)#q', ('"id 1"', "'id 1'"))
         else:
-            test('%(title4)q', ('\'foo "bar" test\'', "'foo 'bar' test'"))
+            test('%(title4).10S', ('foo "bar" ', 'foo "bar" '))
+            test('%(title4)q', ('\'foo "bar" test\'', "'foo \"bar\" test'"))
             test('%(formats.:.id)#q', "'id 1' 'id 2' 'id 3'")
             test('%(formats.0.id)#q', "'id 1'")
 
@@ -809,7 +818,10 @@ class TestYoutubeDL(unittest.TestCase):
                           for f in FORMATS])
         test('%(formats.:.{id,height.:2})j', (out, sanitize(out)))
         test('%(formats.:.{id,height}.id)l', ', '.join(f['id'] for f in FORMATS))
-        test('%(.{id,title})j', ('{"id": "1234"}', "{'id' - '1234'}"))
+        if compat_os_name == 'nt':
+            test('%(.{id,title})j', ('{"id": "1234"}', "{'id' - '1234'}"))
+        else:
+            test('%(.{id,title})j', '{"id": "1234"}')
 
         # Alternates
         test('%(title,id)s', '1234')
@@ -849,8 +861,13 @@ class TestYoutubeDL(unittest.TestCase):
         # Path expansion and escaping
         test('Hello %(title1)s', 'Hello $PATH')
         test('Hello %(title2)s', 'Hello %PATH%')
-        test('%(title3)s', ('foo/bar\\test', 'foo_bar_test'))
-        test('folder/%(title3)s', ('folder/foo/bar\\test', 'folder%sfoo_bar_test' % os.path.sep))
+
+        if compat_os_name == 'nt':
+            test('%(title3)s', ('foo/bar\\test', 'foo_bar_test'))
+            test('folder/%(title3)s', ('folder/foo/bar\\test', 'folder%sfoo_bar_test' % os.path.sep))
+        else:
+            test('%(title3)s', ('foo/bar\\test', 'foo_bar\\test'))
+            test('folder/%(title3)s', ('folder/foo/bar\\test', 'folder%sfoo_bar\\test' % os.path.sep))
 
     def test_format_note(self):
         ydl = YoutubeDL()
