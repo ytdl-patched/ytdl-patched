729d965c448fad14f10ea3f2ae98fd21b8297914
Lesmiscore <nao20010128@gmail.com>
2023-03-20 23:30:04 +0900

    extractor/iwara: detect private videos
    ref. https://github.com/yt-dlp/yt-dlp/issues/3852

--- yt_dlp/extractor/iwara.py
+++ yt_dlp/extractor/iwara.py
@@ -1,9 +1,12 @@
 import functools
+import urllib.error
 import urllib.parse
 import hashlib
+import json
 
 from .common import InfoExtractor
 from ..utils import (
+    ExtractorError,
     OnDemandPagedList,
     int_or_none,
     mimetype2ext,
@@ -75,7 +78,20 @@ class IwaraIE(InfoExtractor):
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        video_data = self._download_json(f'http://api.iwara.tv/video/{video_id}', video_id)
+        try:
+            video_data = self._download_json(f'http://api.iwara.tv/video/{video_id}', video_id)
+        except ExtractorError as e:
+            if isinstance(e.cause, urllib.error.HTTPError) and e.cause.code == 403:
+                # at this point we can actually get uploaded user info, but do we need it?
+                try:
+                    errresp = json.load(e.cause, strict=False)
+                except json.JSONDecodeError:
+                    raise e
+                errmsg = traverse_obj(errresp, 'message')
+                if errmsg == 'errors.privateVideo':
+                    self.raise_login_required('Private video. Login if you have permissions to watch')
+                else:
+                    raise ExtractorError(f'Iwara says: {errmsg}')
 
         return {
             'id': video_id,
